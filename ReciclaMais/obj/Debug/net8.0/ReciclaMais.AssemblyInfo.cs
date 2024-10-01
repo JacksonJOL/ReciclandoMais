d785cf139f0fe13578c1af0635dbc2e63d729df4
JacksonJOL <jacksonoliveira.jl@gmail.com>
2024-10-01 15:13:01 -0300

    Atualização 01/10 15:12

--- ReciclaMais/obj/Debug/net8.0/ReciclaMais.AssemblyInfo.cs
+++ ReciclaMais/obj/Debug/net8.0/ReciclaMais.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReciclaMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1ebe8016f95b1bb12eb8d93c483d6dcf44e858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905664fd7205982ead8ae5cf7498a648728be20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReciclaMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReciclaMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
